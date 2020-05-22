2b7a391e6bc46dfc50e200ba3c7def0790f653fa
Magmusacy <xpfk.098@gmail.com>
2020-05-22 17:38:00 +0200

    While loop should now work properly

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,30 +1,36 @@
 require_relative 'board'
-board = Board.new([[1,2,3],[4,5,6],[7,8,9]])
-###############
-p board.upr_row
-board.midl_row[1] = "X"
-board.botm_row[0] = "O"
-board.upr_row = ["O","O","O"]
-p board.midl_row
-p board.botm_row
-board.show_board
-################
+require 'pry'
+board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
 puts "Welcome to Tic Tac Toe\n".center(60)
 print "Please enter a name for player one |X|: "
   player_one_name = gets.chomp
   player_one = PlayerOne.new(player_one_name)
-  p player_one.name
-  p player_one.sign
 print "Please enter a name for player two |O|: "
   player_two_name = gets.chomp
   player_two = PlayerTwo.new(player_two_name)
-  p player_two.name
-  p player_two.sign
 match_in_progress = true
-p "elo mordo"
+puts "The match has begun!"
+board.show_board
   while match_in_progress do
-    p board.check_for_win(player_two)
-    gets
+    puts "#{player_one.name}'s turn"
+    integer_move = gets.chomp.to_i
+      until board.square.include?(integer_move) do
+        puts "This position isn't present or was already taken"
+        integer_move = gets.chomp.to_i
+      end
+    player_one.move(board,integer_move)
+      board.check_for_win(player_one)
+    board.show_board
+    puts "#{player_two.name}'s turn"
+    integer_move = gets.chomp.to_i
+      until board.square.include?(integer_move) do
+        puts "This position isn't present or was already taken"
+        integer_move = gets.chomp.to_i
+      end
+    player_two.move(board,integer_move)
+      board.check_for_win(player_one)
+    board.show_board
   end
 
 
