1a5d85fe4c86de5ea932c2732f9fa78be4a36f29
Magmusacy <xpfk.098@gmail.com>
2020-05-22 17:34:21 +0200

    Added move method

--- player_one.rb
+++ player_one.rb
@@ -1,9 +1,12 @@
-require 'pry'
 class PlayerOne
   attr_reader :sign
   attr_accessor :name
   def initialize(name)
     @name = name
     @sign = "X"
+  end
+
+  def move(instance,num)
+    instance.square[instance.square.index(num)] = sign 
   end
 end
