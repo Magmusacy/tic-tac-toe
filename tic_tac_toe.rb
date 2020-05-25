33d07d101c822ba84be52498788e37d2f963a525
Magmusacy <xpfk.098@gmail.com>
2020-05-25 13:36:35 +0200

    The game will now exit when one player wins

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,4 @@
 require_relative 'board'
-require 'pry'
 board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 puts "Welcome to Tic Tac Toe\n".center(60)
@@ -15,22 +14,37 @@ board.show_board
   while match_in_progress do
     puts "#{player_one.name}'s turn"
     integer_move = gets.chomp.to_i
+
       until board.square.include?(integer_move) do
         puts "This position isn't present or was already taken"
         integer_move = gets.chomp.to_i
       end
+
     player_one.move(board,integer_move)
-      board.check_for_win(player_one)
+      match_in_progress = board.check_for_win(player_one)
     board.show_board
+
+      if match_in_progress == false
+        puts "#{player_two.name} has won this match! Congratulations"
+        break
+      end
     puts "#{player_two.name}'s turn"
     integer_move = gets.chomp.to_i
+
       until board.square.include?(integer_move) do
         puts "This position isn't present or was already taken"
         integer_move = gets.chomp.to_i
       end
+      
     player_two.move(board,integer_move)
-      board.check_for_win(player_one)
+      match_in_progress = board.check_for_win(player_two)
     board.show_board
+    
+      if match_in_progress == false
+        puts "#{player_two.name} has won this match! Congratulations"
+        break
+      end
   end
+
 
 
