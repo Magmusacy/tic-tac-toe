812cf4ab9806f31a766e65f0082583ee476ae7a0
Magmusacy <xpfk.098@gmail.com>
2020-07-29 13:10:50 +0200

    Slightly refactored the code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,16 @@
 require_relative 'board'
 board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
+def make_move(board)
+  move = gets.chomp.to_i
+  unless board.square.include?(move)
+    puts "This position isn't present or was already taken" 
+    make_move(board)
+  else
+    move
+  end
+end
+
 puts "Welcome to Tic Tac Toe\n".center(60)
 print "Please enter a name for player one |X|: "
   player_one_name = gets.chomp
@@ -11,40 +21,31 @@ print "Please enter a name for player two |O|: "
 match_in_progress = true
 puts "The match has begun!"
 board.show_board
-  while match_in_progress do
-    puts "#{player_one.name}'s turn"
-    integer_move = gets.chomp.to_i
 
-      until board.square.include?(integer_move) do
-        puts "This position isn't present or was already taken"
-        integer_move = gets.chomp.to_i
-      end
+  until match_in_progress == "draw" || match_in_progress == false do
+    puts "#{player_one.name}'s turn"
+    integer_move = make_move(board)
 
     player_one.move(board,integer_move)
-      match_in_progress = board.check_for_win(player_one)
+      match_in_progress = board.check_for_win(player_one.sign)
     board.show_board
 
-      if match_in_progress == false
-        puts "#{player_one.name} has won this match! Congratulations"
-        break
-      end
-    puts "#{player_two.name}'s turn"
-    integer_move = gets.chomp.to_i
+    case match_in_progress
+      when false then puts "#{player_two.name} has won this match! Congratulations"; break
+      when "draw" then puts "Draw! Nobody is a winner"; break
+    end
 
-      until board.square.include?(integer_move) do
-        puts "This position isn't present or was already taken"
-        integer_move = gets.chomp.to_i
-      end
+    puts "#{player_two.name}'s turn"
+    integer_move = make_move(board)
 
     player_two.move(board,integer_move)
-      match_in_progress = board.check_for_win(player_two)
+    p player_two
+      match_in_progress = board.check_for_win(player_two.sign)
     board.show_board
 
-      if match_in_progress == false
-        puts "#{player_two.name} has won this match! Congratulations"
-        break
+      case match_in_progress
+        when false then puts "#{player_two.name} has won this match! Congratulations"; break
+        when "draw" then puts "Draw! Nobody is a winner"; break
       end
+
   end
-
-
-
