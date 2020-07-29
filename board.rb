ba3f9dd0885f4f1056011c471873821e4a6dcdb8
Magmusacy <xpfk.098@gmail.com>
2020-07-29 13:12:28 +0200

    Added an edge case for draw

--- board.rb
+++ board.rb
@@ -5,26 +5,28 @@ class Board
   def initialize(square)
     @square = square
   end
-  
+
   def show_board
     width = 60
     puts
     puts "#{square[0]}|#{square[1]}|#{square[2]}".center(width)
     puts "#{square[3]}|#{square[4]}|#{square[5]}".center(width)
     puts "#{square[6]}|#{square[7]}|#{square[8]}".center(width)
-    puts 
-  end
-  
-  def check_for_win(p)
-    POSSIBLE_WINS.each do |set|
-      return false if square[set[0]] == p.sign && square[set[1]] == p.sign && square[set[2]] == p.sign 
-    end
+    puts
   end
 
-  protected
-  
-  POSSIBLE_WINS = [ [0,1,2],[3,4,5],[6,7,8],
-                    [0,3,6],[1,4,7],[1,4,7],
-                    [2,5,8],[0,4,8],[2,4,6] 
-                  ] # contains indexes of all possible win combinations
+  def check_for_win(p)
+    POSSIBLE_WINS.each do |set|
+      return false if square[set[0]] == p && square[set[1]] == p && square[set[2]] == p
+    end
+    check_for_draw
+  end
+
+  def check_for_draw
+    "draw" if square.none? { |idx| idx.class == Integer} 
+  end
+
+  POSSIBLE_WINS = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                   [0, 3, 6], [1, 4, 7], [1, 4, 7],
+                   [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze # contains indexes of all possible win combinations
 end
